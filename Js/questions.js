b06ba074adafb3b2ddebe7738793633fb3f0ffac
Al-Ameen Babawale <babawaleolatunji64@gmail.com>
2022-02-15 21:48:12 +0100

    first commit

---
+++ Js/questions.js
@@ -0,0 +1,241 @@
+
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Everybody _____ Raymond.",
+    answer: "Loves",
+    options: [
+      "Loved",
+      "Loves",
+      "Love",
+      "Loving"
+    ]
+  },
+    {
+    numb: 2,
+    question: "We ____ won it all!",
+    answer: "Have",
+    options: [
+      "Have",
+      "Had",
+      "Has",
+      "Haven"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Neither Tiana nor I _____ coming with you.",
+    answer: "Am",
+    options: [
+      "Will",
+      "Am",
+      "Is",
+      "Would"
+    ]
+  },
+    {
+    numb: 4,
+    question: "A jar of hearts ____ on the counter.",
+    answer: "Is",
+    options: [
+      "Are",
+      "Were",
+      "Is",
+      "Will"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Each phone ____ costs $168.",
+    answer: "Costs",
+    options: [
+      "Cost",
+      "Costs",
+      "Corst",
+      "Corts"
+    ]
+  },
+  
+    {
+    numb: 6,
+    question: "There are many other ___ to support my point.",
+    answer: "Examples",
+    options: [
+      "Example",
+      "Ecxample",
+      "Examples",
+      "Exmples"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Where are the cookies that ___ in the cookie jar?",
+    answer: "Were",
+    options: [
+      "Was",
+      "Were",
+      "Are",
+      "Is"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Each of the three little bags ___ stolen.",
+    answer: "Was",
+    options: [
+      "Was",
+      "Were",
+      "Is",
+      "Are"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Every Bently, Lamborghini, and Porche here ___ owned by him.",
+    answer: "Is",
+    options: [
+      "Is",
+      "Are",
+      "Was",
+      "Will"
+    ]
+  },
+
+
+      {
+    numb: 10,
+    question: "Dayo ___ to code",
+    answer: "Loves",
+    options: [
+      "Loves",
+      "Love",
+      "Loved",
+      "Loving"
+    ]
+  },
+
+    {
+    numb: 11,
+    question: " If 3x + 3y = 4 what is the value of 21x + 21y?",
+    answer: "4/9",
+    options: [
+      "36",
+      "24",
+      "4/9",
+      "9/4"
+    ]
+  },
+    {
+    numb: 12,
+    question: "What is the cubic root of 64",
+    answer: "4",
+    options: [
+      "14",
+      "4",
+      "16",
+      "2"
+    ]
+  },
+
+    {
+    numb: 13,
+    question: "The x axis on the carteesian plane is?",
+    answer: "Horizontal",
+    options: [
+      "Vetical",
+      "Diagonal",
+      "Horizontal",
+      "Not a line"
+    ]
+  },
+
+    {
+    numb: 14,
+    question: "Angle at a point measures?",
+    answer: "360 deg",
+    options: [
+      "180 deg",
+      "720 deg",
+      "360 deg",
+      "90 deg"
+    ]
+  },
+
+    {
+    numb: 15,
+    question: "The log of 64 to the base of 2 is?",
+    answer: "6",
+    options: [
+      "7",
+      "5",
+      "6",
+      "8"
+    ]
+  },
+  
+      {
+    numb: 16,
+    question: "If x equals 30, what is the half of x squared",
+    answer: "225",
+    options: [
+      "900",
+      "225",
+      "169",
+      "144"
+    ]
+  },
+
+    {
+    numb: 17,
+    question: "What is the fourth term of the geometric progression 32,16,8...",
+    answer: "4",
+    options: [
+      "6",
+      "4",
+      "8",
+      "2"
+    ]
+  },
+
+
+
+      {
+    numb: 18,
+    question: "What is 2 plus 2?",
+    answer: "4",
+    options: [
+      "22",
+      "2",
+      "4",
+      "44"
+    ]
+  },
+
+
+      {
+    numb: 19,
+    question: "The longest side of a right angled triangle is called the?",
+    answer: "Hypotenus",
+    options: [
+      "Quadrant",
+      "Hypotenus",
+      "Diagonal",
+      "Slope"
+    ]
+  },
+
+    {
+    numb: 20,
+    question: "What is the value of 105:15",
+    answer: "7",
+    options: [
+      "15",
+      "6",
+      "7",
+      "8"
+    ]
+  },
+
+   ];
+  
